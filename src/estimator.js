22cc8a52fda475068d4c1e700f4dca309b02107c
budz007 <boudii30@gmail.com>
2020-04-13 19:39:59 +0300

    Update estimator.js
    complete

--- src/estimator.js
+++ src/estimator.js
@@ -50,7 +50,46 @@ let estimate;
     
     
     
-    return { data, impact, severeImpact };
+    severeImpact.currentlyInfected = casesOf(reportedCases, 50);
+    severeImpact.infectionsByRequestedTime = casesOf(
+      severeImpact.currentlyInfected,
+      result
+    );
+  
 
-}
-    module.exports = covid19ImpactEstimator;
+    let severeImpactCases = casesOf(
+      severeImpact.infectionsByRequestedTime,
+      0.15
+    );
+    severeImpactCases = Math.trunc(severeImpactCases);
+    const severeCasesForICU = casesOf(
+      severeImpact.infectionsByRequestedTime,
+      0.05
+    );
+  
+    const severeCasesForVentilators = casesOf(
+      severeImpact.infectionsByRequestedTime,
+      0.02
+    );
+  
+    const severePopIncome = avgDailyIncomePopulation * avgDailyIncomeInUSD;
+    const severeMoneyLoss = (severeImpact.infectionsByRequestedTime * severePopIncome) / estimate;
+  
+    let severeImpactHospitalBeds = casesOf(totalHospitalBeds, 0.35);
+    severeImpactHospitalBeds = Math.trunc(
+      severeImpactHospitalBeds - severeImpactCases
+    );
+    severeImpact.severeCasesByRequestedTime = severeImpactCases;
+    severeImpact.hospitalBedsByRequestedTime = severeImpactHospitalBeds;
+    severeImpact.casesForICUByRequestedTime = Math.trunc(severeCasesForICU);
+    severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(severeCasesForVentilators);
+    severeImpact.dollarsInFlight = Math.trunc(severeMoneyLoss);
+  
+
+    return { data, impact, severeImpact };
+  };
+  
+
+  module.exports = covid19ImpactEstimator;
+
+
