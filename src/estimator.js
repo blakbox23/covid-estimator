b3ffb0d039db02782a16ec7d846ccdca2205005b
budz007 <boudii30@gmail.com>
2020-04-13 14:41:14 +0300

    part

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,16 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => (
+    {
+        data,
+        impact: {
+            currentlyInfected: data.reportedCases*10,
+            infectionsByRequestedTime: currentlyInfected*1024
+        },
+        servereImpact: {
+            currentlyInfected: data.reportedCases*50,
+            infectionsByRequestedTime: this.currentlyInfected*1024
+        }
+    }
+)
+
 
 export default covid19ImpactEstimator;
